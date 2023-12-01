f77b5e5aebdb32f74ddd8c1883b35ea75510c8c2
haoping674 <F110151105@nkust.edu.tw>
2023-12-01 14:38:18 +0800

    first commit

---
+++ FakeDataGenerator/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace FakeDataGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            register = new Button();
+            randomTransaction = new Button();
+            splitContainer1 = new SplitContainer();
+            label2 = new Label();
+            recommend = new TextBox();
+            label1 = new Label();
+            registerName = new TextBox();
+            transactionRecord = new ListBox();
+            login = new Button();
+            label5 = new Label();
+            recommenderLabel = new Label();
+            loginName = new TextBox();
+            codeLabel = new Label();
+            randomUser = new Button();
+            randomRecommender = new Button();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // register
+            // 
+            register.Location = new Point(4, 69);
+            register.Name = "register";
+            register.Size = new Size(300, 32);
+            register.TabIndex = 0;
+            register.Text = "註冊";
+            register.UseVisualStyleBackColor = true;
+            register.Click += register_Click;
+            // 
+            // randomTransaction
+            // 
+            randomTransaction.Location = new Point(648, 262);
+            randomTransaction.Name = "randomTransaction";
+            randomTransaction.Size = new Size(145, 85);
+            randomTransaction.TabIndex = 1;
+            randomTransaction.Text = "隨機產生交易";
+            randomTransaction.UseVisualStyleBackColor = true;
+            randomTransaction.Click += randomTransaction_Click;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(12, 12);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(label2);
+            splitContainer1.Panel1.Controls.Add(recommend);
+            splitContainer1.Panel1.Controls.Add(label1);
+            splitContainer1.Panel1.Controls.Add(registerName);
+            splitContainer1.Panel1.Controls.Add(register);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(login);
+            splitContainer1.Panel2.Controls.Add(label5);
+            splitContainer1.Panel2.Controls.Add(loginName);
+            splitContainer1.Size = new Size(630, 114);
+            splitContainer1.SplitterDistance = 317;
+            splitContainer1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(4, 39);
+            label2.Name = "label2";
+            label2.Size = new Size(105, 19);
+            label2.TabIndex = 4;
+            label2.Text = "Recommend :";
+            // 
+            // recommend
+            // 
+            recommend.Location = new Point(115, 36);
+            recommend.Name = "recommend";
+            recommend.Size = new Size(188, 27);
+            recommend.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(4, 6);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 19);
+            label1.TabIndex = 2;
+            label1.Text = "Username :";
+            // 
+            // registerName
+            // 
+            registerName.Location = new Point(97, 3);
+            registerName.Name = "registerName";
+            registerName.Size = new Size(206, 27);
+            registerName.TabIndex = 1;
+            // 
+            // transactionRecord
+            // 
+            transactionRecord.FormattingEnabled = true;
+            transactionRecord.ItemHeight = 19;
+            transactionRecord.Location = new Point(12, 235);
+            transactionRecord.Name = "transactionRecord";
+            transactionRecord.Size = new Size(623, 194);
+            transactionRecord.TabIndex = 7;
+            // 
+            // login
+            // 
+            login.Location = new Point(3, 36);
+            login.Name = "login";
+            login.Size = new Size(300, 32);
+            login.TabIndex = 5;
+            login.Text = "登入";
+            login.UseVisualStyleBackColor = true;
+            login.Click += login_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 6);
+            label5.Name = "label5";
+            label5.Size = new Size(87, 19);
+            label5.TabIndex = 6;
+            label5.Text = "Username :";
+            // 
+            // recommenderLabel
+            // 
+            recommenderLabel.AutoSize = true;
+            recommenderLabel.BackColor = SystemColors.Control;
+            recommenderLabel.Location = new Point(12, 203);
+            recommenderLabel.Name = "recommenderLabel";
+            recommenderLabel.Size = new Size(156, 19);
+            recommenderLabel.TabIndex = 6;
+            recommenderLabel.Text = "Your recommender : ";
+            // 
+            // loginName
+            // 
+            loginName.Location = new Point(96, 3);
+            loginName.Name = "loginName";
+            loginName.Size = new Size(206, 27);
+            loginName.TabIndex = 5;
+            // 
+            // codeLabel
+            // 
+            codeLabel.AutoSize = true;
+            codeLabel.Location = new Point(12, 171);
+            codeLabel.Name = "codeLabel";
+            codeLabel.Size = new Size(160, 19);
+            codeLabel.TabIndex = 5;
+            codeLabel.Text = "Your invitation code : ";
+            // 
+            // randomUser
+            // 
+            randomUser.Location = new Point(648, 353);
+            randomUser.Name = "randomUser";
+            randomUser.Size = new Size(145, 85);
+            randomUser.TabIndex = 3;
+            randomUser.Text = "隨機產生用戶";
+            randomUser.UseVisualStyleBackColor = true;
+            randomUser.Click += randomUser_Click;
+            // 
+            // randomRecommender
+            // 
+            randomRecommender.Location = new Point(648, 171);
+            randomRecommender.Name = "randomRecommender";
+            randomRecommender.Size = new Size(145, 85);
+            randomRecommender.TabIndex = 4;
+            randomRecommender.Text = "隨機產生推薦人";
+            randomRecommender.UseVisualStyleBackColor = true;
+            randomRecommender.Click += randomRecommender_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(transactionRecord);
+            Controls.Add(randomRecommender);
+            Controls.Add(recommenderLabel);
+            Controls.Add(randomUser);
+            Controls.Add(codeLabel);
+            Controls.Add(splitContainer1);
+            Controls.Add(randomTransaction);
+            Name = "Form1";
+            Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button register;
+        private Button randomTransaction;
+        private SplitContainer splitContainer1;
+        private Label label1;
+        private TextBox registerName;
+        private Button randomUser;
+        private Label label2;
+        private TextBox recommend;
+        private Label codeLabel;
+        private Label recommenderLabel;
+        private Button randomRecommender;
+        private Button login;
+        private Label label5;
+        private TextBox loginName;
+        private ListBox transactionRecord;
+    }
+}
---
+++ FakeDataGenerator/Form1.cs
@@ -0,0 +1,239 @@
+﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Common;
+using System.Diagnostics.Tracing;
+using static System.TimeZoneInfo;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace FakeDataGenerator
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Server=127.0.0.1;Database=test;Uid=root;Pwd=root;";
+        private MySqlConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private void register_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                string name = registerName.Text;
+                if (CheckName(name))
+                {
+                    string recommend_code = recommend.Text;
+                    string code = GenerateInvitationCode(new Random());
+                    DateTime date = DateTime.Now;
+                    int agentFk = GetRecommenderpk(recommend_code);
+                    string query = $"INSERT INTO member (username, invitation_code, recommendmember, agent_fk, create_time) " +
+                             $"VALUES ('{name}', '{code}', '{recommend_code}', {agentFk}, " +
+                             $"'{date.ToString("yyyy-MM-dd HH:mm:ss")}')";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("註冊成功!!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("註冊失敗!!");
+                Console.WriteLine(ex.ToString());
+            }
+            connection.Close();
+        }
+
+        private void randomTransaction_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Random random = new Random();
+                connection.Open();
+                string query = "SELECT COUNT(*) FROM member";
+                MySqlCommand countCommand = new MySqlCommand(query, connection);
+                int total = Convert.ToInt32(countCommand.ExecuteScalar());
+                for (int i = 0; i < 2500; i++)
+                {
+                    int pk = random.Next(1, total + 1);
+                    (int transactionType, DateTime transactiondate) = GenerateTransactionDate(random, pk);
+                    decimal transactionAmount = (decimal)random.NextDouble() * 10000;
+                    string insertQuery = $"INSERT INTO borrow_fee (member_fk, type, borrow_fee, create_time) " +
+                             $"VALUES ({pk}, {transactionType}, {transactionAmount}, " +
+                             $"'{transactiondate.ToString("yyyy-MM-dd HH:mm:ss")}')";
+                    MySqlCommand command = new MySqlCommand(insertQuery, connection);
+                    command.ExecuteNonQuery();
+                }
+                MessageBox.Show("隨機產生交易成功!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("隨機產生交易失敗!!");
+                Console.WriteLine(ex.ToString());
+            }
+            connection.Close();
+        }
+
+        private void randomUser_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                Random random = new Random();
+                for (int i = 0; i < 1000; i++)
+                {
+                    string code = GenerateInvitationCode(random);
+                    DateTime date = GenerateRegistrationDate(random);
+                    string name = $"User_{i + 1}";
+                    string query = $"INSERT INTO member (username, invitation_code, recommendmember, create_time) " +
+                                $"VALUES ('{name}', '{code}', NULL, '{date.ToString("yyyy-MM-dd HH:mm:ss")}')";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    command.ExecuteNonQuery();
+                }
+                MessageBox.Show("隨機產生用戶成功!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("隨機產生用戶失敗!!");
+                Console.WriteLine(ex.ToString());
+            }
+            connection.Close();
+        }
+
+        private void randomRecommender_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                string query = "SELECT invitation_code FROM member ORDER BY RAND() LIMIT 1";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    recommend.Text = reader.GetString(0);
+                }
+                reader.Close();
+                MessageBox.Show("隨機產生推薦人成功!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("隨機產生推薦人失敗!!");
+                Console.WriteLine(ex.ToString());
+            }
+            connection.Close();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                string query = $"SELECT pk, agent_fk, invitation_code FROM member WHERE username = '{loginName.Text}'";
+
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+                string code = "";
+                string recommender = "None";
+                int agentFk = 0;
+                int pk = 0;
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    pk = reader.GetInt32(0);
+                    agentFk = reader.GetInt32(1);
+                    code = reader.GetString(2);
+                }
+                reader.Close();
+
+                if (agentFk != 0)
+                {
+                    query = $"SELECT username FROM member WHERE pk = {agentFk}";
+                    command = new MySqlCommand(query, connection);
+                    recommender = command.ExecuteScalar().ToString();
+                }
+                DisplayRecord(pk);
+                recommenderLabel.Text = "Your recommender : " + recommender;
+                codeLabel.Text = "Your invitation code : " + code;
+                MessageBox.Show("登入成功!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("登入失敗!!");
+                Console.WriteLine(ex.ToString());
+            }
+            connection.Close();
+        }
+
+        private string GenerateInvitationCode(Random random)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 7)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private DateTime GenerateRegistrationDate(Random random)
+        {
+            DateTime start = new DateTime(2023, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(random.Next(range));
+        }
+
+        private (int, DateTime) GenerateTransactionDate(Random random, int member_pk)
+        {
+            int type = 1;
+            DateTime start = GetRegistrationDate(member_pk);
+            string query = $"SELECT MIN(create_time) FROM borrow_fee WHERE member_fk = {member_pk}";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            object result = command.ExecuteScalar();
+            if (result != null && result != DBNull.Value)
+            {
+                start = Convert.ToDateTime(result);
+                type = 2;
+            }
+            int range = (DateTime.Today - start).Days;
+            return (type, start.AddDays(random.Next(range)));
+        }
+
+        private int GetRecommenderpk(string code)
+        {
+            string query = $"SELECT pk FROM member WHERE invitation_code = '{code}'";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            return Convert.ToInt32(command.ExecuteScalar());
+        }
+
+        private DateTime GetRegistrationDate(int pk)
+        {
+            string query = $"SELECT create_time FROM member WHERE pk = {pk}";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            return Convert.ToDateTime(command.ExecuteScalar());
+        }
+
+        private bool CheckName(string name)
+        {
+            string query = $"SELECT COUNT(*) FROM member WHERE username = '{name}'";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            return Convert.ToInt32(command.ExecuteScalar()) == 0;
+        }
+
+        private void DisplayRecord(int member_pk)
+        {
+            string query = $"SELECT borrow_fee.borrow_fee, borrow_fee.create_time, borrow_fee.type FROM borrow_fee " +
+                           $"INNER JOIN member ON borrow_fee.member_fk = member.pk " +
+                           $"WHERE member.pk = '{member_pk}'" + 
+                           $"ORDER BY borrow_fee.create_time DESC, borrow_fee.pk DESC"; ;
+            MySqlCommand command = new MySqlCommand(query, connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            transactionRecord.Items.Clear();
+            while (reader.Read())
+            {
+                decimal borrowFee = reader.GetDecimal(0);
+                DateTime createTime = reader.GetDateTime(1);
+                int type = reader.GetInt32(2);
+                transactionRecord.Items.Add($"Borrow Fee: {borrowFee}, Date: {createTime}, Type: {type}");
+            }
+            reader.Close();
+        }
+    }
+}
